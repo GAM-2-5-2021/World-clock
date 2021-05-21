d0abd5d21c725167817fc43b8d6608af0c3759aa
Tara-2 <84178723+Tara-2@users.noreply.github.com>
2021-05-21 18:02:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/') #dodaje glavnu rutu koja ce se loadoat pri otvaranju stranice
+@app.route('/') 
 def main():
-    return render_template('index.html') #funkcija vraca galvni html file
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=80) #stavlja ip za host-a i odreduje port te pokrece stranicu(aplikaciju)
+    app.run(debug=False, host='0.0.0.0', port=80) 
